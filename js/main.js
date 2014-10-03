84d5cf1eabe2b79fbbb3caf89862f019fd884676
Jared Rosen <jaredarosen@gmail.com>
2014-10-02 22:53:06 -0400

    color

--- js/main.js
+++ js/main.js
@@ -18,10 +18,10 @@ function set_tabs() {
 }
 
 function toggle_tabs() {
-	$(".tab-nav a").click(function(){
+	$(".tab-nav a").click(function(e){
+		e.preventDefault();
 		$(".tab-panel").hide();
 		$($(this).attr('href')).show();
-
 		$(".tab-nav a").css({'font-weight':'normal'});
 		$($(".tab-nav a[href=\""+$(this).attr("href")+"\"]")).css({'font-weight':'bold'});
 	});
